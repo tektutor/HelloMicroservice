3eddd7fa624226231a224d541a75d382fbba97dc
Jeganathan Swaminathan <jegan@tektutor.org>
2021-04-30 22:55:01 +0530

    Update HelloController.java

--- src/main/java/org/tektutor/HelloController.java
+++ src/main/java/org/tektutor/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	@GetMapping("hello")
+	@GetMapping("/")
 	public String sayHello() {
 		return "Hello World 1.0";
 	}
